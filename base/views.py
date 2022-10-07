88cbc7bae1742f2915c624ab465d22078ec543ce
zephyr72836 <zephyr72836@gmail.com>
2022-10-07 17:45:05 +0900

    검색 기능 추가

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
@@ -7,9 +8,24 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q= request.GET.get('q') if request.GET.get('q') != None else ''
+
+    # icontains: 포함하도록 나중에 찾아보자,,,
+    # Q 동적 쿼리 메소드 ... 나중에 찾아보자,,,
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+    )
+    # 나중에 필터로 상위 n개만 걸러내는 기능 추가하기
+    topics = Topic.objects.all()
+    # len도 가능한데 len보다 이게 나음
+    room_count = rooms.count()
+
     context = {
         'rooms' : rooms,
+        'topics': topics,
+        'room_count': room_count,
     }
     return render(request, 'base/home.html', context)
 
