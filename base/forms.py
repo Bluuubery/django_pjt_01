bf3d3276bd137f820f54fd07d68f7c9adf165327
SunJun-SL <zephyr72836@gmail.com>
2022-10-08 21:01:38 +0900

    static 요소 추가, create 수정

--- base/forms.py
+++ base/forms.py
@@ -5,4 +5,6 @@ class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
+        exclude = ['host', 'participants']
+        
         # fields = ['name', 'contents',] 와 같이 특정 요소만을 불러올 수 잇음
